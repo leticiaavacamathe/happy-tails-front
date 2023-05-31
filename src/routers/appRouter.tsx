ef3e503f3b6739bfc3ef54995e5a20033dd87927
Leticia Avaca Mathe <leticia_a.m@hotmail.com>
2023-05-31 17:02:19 +0200

    Add login path that redirects to LoginPage in appRouter

--- src/routers/appRouter.tsx
+++ src/routers/appRouter.tsx
@@ -1,11 +1,26 @@
 import { RouteObject } from "react-router";
 import App from "../components/App/App";
-import { createBrowserRouter } from "react-router-dom";
+import { Navigate, createBrowserRouter } from "react-router-dom";
+import LoginPage from "../pages/LoginPage/LoginPage";
 
 const routes: RouteObject[] = [
   {
     path: "/",
     element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Navigate to="/login" replace />,
+      },
+      {
+        path: "/login",
+        element: <LoginPage />,
+      },
+      {
+        path: "*",
+        element: <p>Page not found</p>,
+      },
+    ],
   },
 ];
 
