a9682ee133ed64509bd92ebf51b6384d10c0526a
Leticia Avaca Mathe <leticia_a.m@hotmail.com>
2023-06-01 11:14:29 +0200

    Add lazyComponent for LoginPage

--- src/components/LoginForm/LoginForm.test.tsx
+++ src/components/LoginForm/LoginForm.test.tsx
@@ -5,9 +5,11 @@ import LoginForm from "./LoginForm";
 describe("Given a LoginForm component", () => {
   describe("When it is rendered", () => {
     test("Then it should show a button with the text 'Log in'", () => {
+      const expectedText = "Log in";
+
       renderWithProviders(wrapWithRouter(<LoginForm />));
 
-      const buttonText = screen.getByRole("button", { name: "Log in" });
+      const buttonText = screen.getByRole("button", { name: expectedText });
 
       expect(buttonText).toBeInTheDocument();
     });
