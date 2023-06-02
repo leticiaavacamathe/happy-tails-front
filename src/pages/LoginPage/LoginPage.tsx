ab8eb8e7a17b0380c3906a55257fe6bf215bc4e0
Leticia Avaca Mathe <leticia_a.m@hotmail.com>
2023-06-02 11:37:22 +0200

    Add loginFormSubmit function in LoginPage

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,6 +1,27 @@
+import { useNavigate } from "react-router-dom";
 import LoginForm from "../../components/LoginForm/LoginForm";
+import { useAppDispatch } from "../../store";
+import { UserCredentials } from "../../store/user/types";
+import { loginUserActionCreator } from "../../store/user/userSlice";
+import useUser from "../../hooks/useUser/useUser";
+import useToken from "../../hooks/useToken/useToken";
 
 const LoginPage = (): React.ReactElement => {
+  const { getTokenData } = useToken();
+  const { getUserToken } = useUser();
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const loginFormSubmit = async (userCredentials: UserCredentials) => {
+    const token = await getUserToken(userCredentials);
+
+    if (token) {
+      const userLoginData = await getTokenData(token);
+      dispatch(loginUserActionCreator(userLoginData));
+      navigate("/home", { replace: true });
+    }
+  };
+
   return (
     <>
       <img
@@ -10,7 +31,7 @@ const LoginPage = (): React.ReactElement => {
         width={150}
         height={180}
       />
-      <LoginForm handleOnSubmit={() => ({})} />
+      <LoginForm handleOnSubmit={loginFormSubmit} />
     </>
   );
 };
