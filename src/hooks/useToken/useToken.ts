7150b29d4bd4b2132bed4842af2924818d2c6710
Leticia Avaca Mathe <leticia_a.m@hotmail.com>
2023-06-02 11:25:57 +0200

    Add useToken custom hook

---
+++ src/hooks/useToken/useToken.ts
@@ -0,0 +1,19 @@
+import jwt_decode from "jwt-decode";
+import { useCallback } from "react";
+
+const useToken = () => {
+  const getTokenData = useCallback((token: string) => {
+    const decodedToken: { sub: string; name: string } = jwt_decode(token);
+    const tokenInfo = {
+      id: decodedToken.sub,
+      name: decodedToken.name,
+      token,
+    };
+
+    return tokenInfo;
+  }, []);
+
+  return { getTokenData };
+};
+
+export default useToken;
