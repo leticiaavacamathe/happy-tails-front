0248bc7ba8dc97ced1237264cd64858ada095b11
Leticia Avaca Mathe <leticia_a.m@hotmail.com>
2023-06-05 12:00:48 +0200

    Add dispatch loadAnimals on AnimalListPage

--- src/pages/AnimalsListPage/AnimalsListPage.tsx
+++ src/pages/AnimalsListPage/AnimalsListPage.tsx
@@ -1,6 +1,13 @@
+import { animalsMocks } from "../../mocks/animalMocks.js";
+import { loadAnimalsActionCreator } from "../../store/animal/animalSlice.js";
+import { useAppDispatch } from "../../store/index.js";
 import AnimalsListPageStyled from "./AnimalsListPageStyled.js";
 
 const AnimalsListPage = (): React.ReactElement => {
+  const dispatch = useAppDispatch();
+
+  dispatch(loadAnimalsActionCreator(animalsMocks));
+
   return (
     <AnimalsListPageStyled>
       <h1 className="list-page-title">Adopt me</h1>
