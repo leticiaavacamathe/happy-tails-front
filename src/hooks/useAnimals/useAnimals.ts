3c983e506fc21d33e71033b8eebeee5ffd84e751
Leticia Avaca Mathe <leticia_a.m@hotmail.com>
2023-06-06 21:04:39 +0200

    Add dispatch hideLoading and showLoading action creator in useAnimals

--- src/hooks/useAnimals/useAnimals.ts
+++ src/hooks/useAnimals/useAnimals.ts
@@ -1,25 +1,36 @@
 import { useCallback } from "react";
-import { useAppSelector } from "../../store";
+import { useAppDispatch, useAppSelector } from "../../store";
 import {
   AnimalDataStructure,
   AnimalsApiResponse,
 } from "../../store/animal/types";
 import axios from "axios";
 import paths from "../../routers/paths";
+import {
+  hideLoadingActionCreator,
+  showLoadingActionCreator,
+} from "../../store/ui/uiSlice";
 
 const apiUrl = import.meta.env.VITE_APP_URL;
 
 const useAnimals = () => {
   const { token } = useAppSelector((state) => state.user);
+  const dispatch = useAppDispatch();
 
   const getAnimals = useCallback(async (): Promise<AnimalDataStructure[]> => {
-    const {
-      data: { animals },
-    } = await axios.get<AnimalsApiResponse>(`${apiUrl}${paths.animals}`, {
-      headers: { Authorization: `Bearer ${token}` },
-    });
-    return animals;
-  }, [token]);
+    try {
+      dispatch(showLoadingActionCreator());
+      const {
+        data: { animals },
+      } = await axios.get<AnimalsApiResponse>(`${apiUrl}${paths.animals}`, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      dispatch(hideLoadingActionCreator());
+      return animals;
+    } catch {
+      throw new Error("Can't get the list of Animals");
+    }
+  }, [dispatch, token]);
   return { getAnimals };
 };
 
