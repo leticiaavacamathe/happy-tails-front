6bd36f6534604b802348049f06af9b2c940c812d
Leticia Avaca Mathe <leticia_a.m@hotmail.com>
2023-06-07 21:40:31 +0200

    Add dispatch showFeedback in useUser hook

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -3,15 +3,18 @@ import ContainerStyled from "../shared/ContainerStyled";
 import Header from "../Header/Header";
 import { useAppSelector } from "../../store";
 import Loader from "../Loader/Loader";
+import Modal from "../Modal/Modal";
 
 const Layout = (): React.ReactElement => {
   const isLoading = useAppSelector((state) => state.ui.isLoading);
+  const isOn = useAppSelector((state) => state.ui.modal.isOn);
 
   const { pathname } = useLocation();
 
   return (
     <>
       {isLoading && <Loader />}
+      {isOn && <Modal />}
       <ContainerStyled>
         {pathname !== "/login" && <Header />}
         <Outlet />
