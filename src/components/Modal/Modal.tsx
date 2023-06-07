6aeaef12c716fe7be4b26c12f31a5fd3b1b6b81e
Leticia Avaca Mathe <leticia_a.m@hotmail.com>
2023-06-07 23:00:53 +0200

    Add handleOnClose action on onClick Modal button

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,17 +1,28 @@
-import { useAppSelector } from "../../store";
+import { useAppDispatch, useAppSelector } from "../../store";
+import { hideFeedbackActionCreator } from "../../store/ui/uiSlice";
 import ModalStyled from "./ModalStyled";
 
 const Modal = (): React.ReactElement => {
-  const { modal } = useAppSelector((state) => state.ui);
+  const { isError, text, title } = useAppSelector((state) => state.ui.modal);
+
+  const dispatch = useAppDispatch();
+  const handleOnClose = () => {
+    dispatch(hideFeedbackActionCreator());
+  };
+
   return (
-    <ModalStyled>
-      <div className={`modal modal${modal.isError ? "--error" : "--correct"}`}>
-        <button className="modal__button" aria-label="exit">
+    <ModalStyled aria-label="feedback modal">
+      <div className={`modal modal${isError ? "--error" : "--correct"}`}>
+        <button
+          className="modal__button"
+          aria-label="exit"
+          onClick={handleOnClose}
+        >
           <img src="images/exit-icon.svg" alt="exit" />
         </button>
         <div className="modal__info">
-          <h3 className="modal__info__title">{modal.title}</h3>
-          <p className="modal__info__text">{modal.text}</p>
+          <h3 className="modal__info__title">{title}</h3>
+          <p className="modal__info__text">{text}</p>
         </div>
       </div>
     </ModalStyled>
--- src/components/Modal/ModalStyled.ts
+++ src/components/Modal/ModalStyled.ts
@@ -1,7 +1,16 @@
 import styled from "styled-components";
 
 const ModalStyled = styled.article`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
   display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.25);
   color: ${(props) => props.theme.colors.fontMainColor};
 
   .modal {
