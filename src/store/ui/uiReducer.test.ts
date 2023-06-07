005295816ddd76c7b6f9eaf711c763cacb8d2442
Leticia Avaca Mathe <leticia_a.m@hotmail.com>
2023-06-07 22:47:14 +0200

    Add showFeedback and hideFeedback reducers tests

--- src/store/ui/uiReducer.test.ts
+++ src/store/ui/uiReducer.test.ts
@@ -1,6 +1,8 @@
 import { UiStructure } from "./types";
 import {
+  hideFeedbackActionCreator,
   hideLoadingActionCreator,
+  showFeedbackActionCreator,
   showLoadingActionCreator,
   uiReducer,
 } from "./uiSlice";
@@ -41,6 +43,53 @@ describe("Given a hideLoading minireducer", () => {
       const newState = uiReducer(currentUiState, hideLoadingActionCreator());
 
       expect(newState).toStrictEqual(expectedUiState);
+    });
+  });
+});
+
+describe("Given a showFeedback minireducer", () => {
+  describe("When it receives an isError property set to true as payload", () => {
+    test("Then it should return a new state with that property set to true", () => {
+      const currentUiState: UiStructure = {
+        modal: { title: "", isError: false, isOn: true, text: "" },
+      };
+
+      const newCurrentUiState: UiStructure = {
+        modal: { title: "", isError: true, text: "", isOn: true },
+      };
+
+      const expectedNewState = uiReducer(
+        currentUiState,
+        showFeedbackActionCreator({
+          isError: true,
+          text: "",
+          title: "",
+          isOn: true,
+        })
+      );
+
+      expect(newCurrentUiState).toStrictEqual(expectedNewState);
+    });
+  });
+});
+
+describe("Given a hideFeedback minireducer", () => {
+  describe("When it has a state with the property isOn set to true", () => {
+    test("Then it should return the new state with the property set to false", () => {
+      const currentUiState: UiStructure = {
+        modal: { isError: false, text: "", title: "", isOn: true },
+      };
+
+      const newCurrentUiState: UiStructure = {
+        modal: { isError: false, text: "", title: "", isOn: false },
+      };
+
+      const expectedNewState = uiReducer(
+        currentUiState,
+        hideFeedbackActionCreator()
+      );
+
+      expect(newCurrentUiState).toStrictEqual(expectedNewState);
     });
   });
 });
