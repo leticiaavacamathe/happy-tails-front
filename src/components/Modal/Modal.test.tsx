f33ed5b864fc3fa3dbb1631252ec2b3f1d508668
Leticia Avaca Mathe <leticia_a.m@hotmail.com>
2023-06-08 10:05:43 +0200

    Add Modal test for onClick close button use case

--- src/components/Modal/Modal.test.tsx
+++ src/components/Modal/Modal.test.tsx
@@ -1,6 +1,7 @@
 import { screen } from "@testing-library/react";
 import { renderWithProviders } from "../../testUtils/testUtils";
 import Modal from "./Modal";
+import userEvent from "@testing-library/user-event";
 
 describe("Given a Modal component", () => {
   describe("When it is rendered", () => {
@@ -22,6 +23,52 @@ describe("Given a Modal component", () => {
       const modal = screen.getByLabelText(text);
 
       expect(modal).toBeInTheDocument();
+    });
+  });
+
+  describe("When it is rendered with a negative feedback", () => {
+    test("Then it should the message 'Incorrect username or password please try again'", () => {
+      const expectedText = "Incorrect username or password please try again";
+
+      renderWithProviders(<Modal />, {
+        ui: {
+          isLoading: true,
+          modal: {
+            isError: true,
+            text: "Incorrect username or password please try again",
+            isOn: true,
+            title: "",
+          },
+        },
+      });
+
+      const feedback = screen.getByText(expectedText);
+
+      expect(feedback).toBeInTheDocument();
+    });
+  });
+
+  describe("When it is rendered and the user clicks on the close button", () => {
+    test("Then it should disappear", async () => {
+      const expectedAltButtonText = "exit";
+
+      renderWithProviders(<Modal />, {
+        ui: {
+          isLoading: true,
+          modal: {
+            isError: true,
+            text: "",
+            isOn: true,
+            title: "",
+          },
+        },
+      });
+      const button = screen.getByRole("button", {
+        name: expectedAltButtonText,
+      });
+      await userEvent.click(button);
+
+      expect(button).toBeInTheDocument();
     });
   });
 });
