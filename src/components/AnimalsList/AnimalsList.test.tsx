1d374e87f91e82407eb7f7d0cdbbc8ffe54d76c2
Leticia Avaca Mathe <leticia_a.m@hotmail.com>
2023-06-09 18:37:53 +0200

    Add deleteOnClick function test

--- src/components/AnimalsList/AnimalsList.test.tsx
+++ src/components/AnimalsList/AnimalsList.test.tsx
@@ -1,24 +1,43 @@
 import { screen } from "@testing-library/react";
-import { animalsMocks } from "../../mocks/animalMocks";
+import { animalMock, animalsMocks } from "../../mocks/animalMocks";
 import { renderWithProviders, wrapWithRouter } from "../../testUtils/testUtils";
 import AnimalsList from "./AnimalsList";
+import userEvent from "@testing-library/user-event";
 
 describe("Given a AnimalsList component", () => {
   describe("When it is rendered and receives an animal card", () => {
     test("Then it should show the name of that card 'max' inside a heading", () => {
-      const expectedText = animalsMocks[0].name;
-
       const animalsStore = {
         animal: {
           animals: animalsMocks,
         },
       };
+      const expectedText = animalsMocks[0].name;
 
       renderWithProviders(wrapWithRouter(<AnimalsList />), animalsStore);
 
       const title = screen.getByRole("heading", { name: expectedText });
 
       expect(title).toBeInTheDocument();
+    });
+  });
+
+  describe("When it is rendered and a delete button is clicked", () => {
+    test("Then it should show a list of animals without the animal deleted", async () => {
+      const animalsStore = {
+        animal: {
+          animals: animalMock,
+        },
+      };
+      renderWithProviders(wrapWithRouter(<AnimalsList />), animalsStore);
+
+      const title = screen.getByRole("heading", { name: "Max" });
+
+      const button = screen.getByRole("button", { name: "delete button" });
+
+      await userEvent.click(button);
+
+      expect(title).not.toBeInTheDocument();
     });
   });
 });
