89f955f6ae7b627b974b403a68e97350b0808e72
Leticia Avaca Mathe <leticia_a.m@hotmail.com>
2023-07-28 17:02:34 +0200

    Add FilterStyled and give styles to Filter

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,9 +1,13 @@
+import FilterStyled from "./FilterStyled";
+
 const Filter = (): React.ReactElement => {
   return (
-    <select className="filter__select" aria-label="filter by category">
-      <option value="Cat">Cat</option>
-      <option value="Dog">Dog</option>
-    </select>
+    <FilterStyled className="filter">
+      <select className="filter__select" aria-label="filter by category">
+        <option value="Cat">Cat</option>
+        <option value="Dog">Dog</option>
+      </select>
+    </FilterStyled>
   );
 };
 
